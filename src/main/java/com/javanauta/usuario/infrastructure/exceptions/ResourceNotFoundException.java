1e5d15e2fd7de4d034b7fd2a2194f8c628d828cf
Nicolas <nicolasruan9000@gmail.com>
2026-04-28 16:10:41 -0300

    correção simples

---
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.javanauta.aprendendo_spring.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String mensagem){
+        super (mensagem);
+    }
+    public ResourceNotFoundException (String mensagem, Throwable throwable){
+        super (mensagem, throwable);
+    }
+}
