5ae4ce5544feea53cf6ff5b59fb483505a7005d9
Nicolas <nicolasruan9000@gmail.com>
2026-04-28 18:14:41 -0300

    teste

--- src/main/java/com/javanauta/usuario/business/UsuarioService.java
+++ src/main/java/com/javanauta/usuario/business/UsuarioService.java
@@ -3,9 +3,12 @@ package com.javanauta.usuario.business;
 import com.javanauta.usuario.business.converter.UsuarioConverter;
 import com.javanauta.usuario.business.dto.UsuarioDTO;
 import com.javanauta.usuario.entity.Usuario;
+import com.javanauta.usuario.infrastructure.exceptions.ConflictException;
 import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,15 +17,36 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     //recebe um objeto usuarioDTO (encriptado)
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
+
         //converte o usuarioDTO para uma usuario entity
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         //RECEBA essa entity e reconverte para DTO e salva esse usuario no repository
         return usuarioConverter.paraUsuarioDTO(
                 usuarioRepository.save(usuario)
         );
+    }
+    public void emailExiste(String email) {
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if (existe) {
+                throw new ConflictException("Email já cadastrado" + email);
+            }
+
+
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado" + e.getCause());
+        }
+
+    }
+
+    public boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
     }
 
 }
--- src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.javanauta.aprendendo_spring.infrastructure.exceptions;
+package com.javanauta.usuario.infrastructure.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String mensagem){
